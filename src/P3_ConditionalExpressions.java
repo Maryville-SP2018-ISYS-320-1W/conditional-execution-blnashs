f0ab72cbe21db7cd621a779882ee718cf4e32b3e
Barbara Nash <bnash1@live.maryville.edu>
2018-04-08 09:56:01 -0500

    string 0

--- src/P3_ConditionalExpressions.java
+++ src/P3_ConditionalExpressions.java
@@ -9,14 +9,14 @@ import java.util.Scanner;
 /*
  Your expected result:
  
- 1.
- 2.
- 3. 
- 4.
+ 1.  true 4
+ 2.  true  4=4  wrong
+ 3.  false 4 doesn't = -3
+ 4.  false -1 isn't greater than 0  wrong
  
- 5.
- 6.
- 7.
+ 5.  false 9 is not less than or equal to 0
+ 6.  true -3/-3 == 1
+ 7.  true -4 > -6
   
  */
 public class P3_ConditionalExpressions {
@@ -27,7 +27,7 @@ public class P3_ConditionalExpressions {
 		int z = 4;
 		
 		// Replace "YOUR EXPRESSION HERE" with the expression you're validating
-		System.out.println("Expression evaluates to: " + ("YOUR EXPRESSION HERE") );
+		System.out.println(x * (y + 2) > y - (y + z) * 2);
 	}
 
 }
